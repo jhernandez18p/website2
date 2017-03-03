38ab754091e81f0ef36c71941ad40aea46d341c2
Josmer Hernández <jhernandez.18p@gmail.com>
2017-03-02 22:13:51 -0500

    DB port

--- settings/settings/production.py
+++ settings/settings/production.py
@@ -12,6 +12,6 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'PORT': '',
     }
 }
