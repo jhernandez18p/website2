8b99e380ce470475aca1f46f191bccd663b443f5
Josmer Hernández <jhernandez.18p@gmail.com>
2017-04-22 16:51:49 -0500

    Google tags Manager

--- staticfiles/base/js/analytics.js
+++ staticfiles/base/js/analytics.js
@@ -1,3 +1,4 @@
+// Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -5,7 +6,17 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-83370429-5', 'auto');
 ga('send', 'pageview');
+// End Google Analytics
 
+// Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WTVZH7M');
+// End Google Tag Manager
+
+// Facebook Analytics
 window.fbAsyncInit = function() {
 	FB.init({
 		appId      : '251445488634550',
@@ -22,3 +33,4 @@ window.fbAsyncInit = function() {
 	js.src = "//connect.facebook.net/es_LA/sdk.js";  
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+// End Facebook Analytics
